68dcdba52c9727dec52075ed9eaa176382f1480c
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-16 19:17:10 -0800

    clocking

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+
+$(document).ready(function() {
+    setInterval(function () {
+        let time = new Date();
+        let t = time.toLocaleTimeString();
+        let date = time.getDate();
+        // let tz = time.timeZoneName();
+        $('#clock').html('<p>' + t + '</p>');
+        $('#date').html('<p>' + time.getMonth()  + time.getUTCDate() + time.getYear() + '</p>');
+        // $('#clock').html('<p class="time">' + t + '</p><p class="date">' + date + '</p><p class="zone">' + tz + '</p>');
+    }, 1000);
+
+});
